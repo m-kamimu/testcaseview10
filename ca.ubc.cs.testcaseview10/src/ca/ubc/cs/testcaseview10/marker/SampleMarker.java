a5fed4102c5089e892e2753ebbf330612e1eedd7
Manabu Kamimura <m-kamimu@nifty.com>
2012-11-24 23:19:21 -0800

    Incomplete - task : Study editor extension (erase annotation source...not needed in this case)

--- ca.ubc.cs.testcaseview10/src/ca/ubc/cs/testcaseview10/marker/SampleMarker.java
+++ ca.ubc.cs.testcaseview10/src/ca/ubc/cs/testcaseview10/marker/SampleMarker.java
@@ -39,56 +39,11 @@ public class SampleMarker {
 	    attributes.put(IMarker.CHAR_START, Integer.valueOf(1000));
 	    attributes.put(IMarker.CHAR_END, Integer.valueOf(1005));
 	    attributes.put(IMarker.MESSAGE, "sample marker");
-	    AbstractMarkerAnnotationModel model =  getModel(resource);
-	    //model.
 	    try {
 			MarkerUtilities.createMarker(resource, attributes, SampleMarker.MARKER_ID);
 		} catch (CoreException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-	    /*
-		       IMarker marker = null;
-		//note: you use the id that is defined in your plugin.xml
-		       marker = resource.createMarker("ca.ubc.cs.testcaseview10.mymarkers");
-		       marker.setAttribute("description," "this is one of my markers");
-		//note: you can also use attributes from your supertype
-		       marker.setAttribute(IMarker.MESSAGE, "My Marker");
-	*/
-	}
-	
-    private static AbstractMarkerAnnotationModel getModel(IResource resource) {
-    	IEditorPart editorp = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-        IDocumentProvider provider = ((ITextEditor)editorp).getDocumentProvider();
-        IAnnotationModel model = provider.getAnnotationModel(editorp.getEditorInput());
-        if (model instanceof AbstractMarkerAnnotationModel) {
-            return (AbstractMarkerAnnotationModel) model;
-        }
-        return null;
-    }
-
-	
-	
-    public static void addAnnotation(IMarker marker, ITextSelection selection, 
-			ITextEditor editor) {
-		//The DocumentProvider enables to get the document currently loaded in the editor
-		IDocumentProvider idp = editor.getDocumentProvider();
-		
-		//This is the document we want to connect to. This is taken from 
-		//the current editor input.
-		IDocument document = idp.getDocument(editor.getEditorInput());
-		
-		//The IannotationModel enables to add/remove/change annotation to a Document 
-		//loaded in an Editor
-		IAnnotationModel iamf = idp.getAnnotationModel(editor.getEditorInput());
-		
-		//Note: The annotation type id specify that you want to create one of your 
-		//annotations
-		SimpleMarkerAnnotation ma = new SimpleMarkerAnnotation("sample.baseMarkerAnnotation",marker);
-		
-		//Finally add the new annotation to the model
-		iamf.connect(document);
-		iamf.addAnnotation(ma,new Position(selection.getOffset(),selection.getLength()));
-		iamf.disconnect(document);
-	} 
+	}	
 }
